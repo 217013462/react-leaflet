079aec0ba057261790990d1b5f6f42a5c80f269a
WY <101268150+217013462@users.noreply.github.com>
2022-05-17 07:55:07 +0000

    Added Traffic News as Alert and Traffic Camera as a new layer

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,47 +1,107 @@
 import React, { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, useMap, Marker, Popup, LayersControl, LayerGroup } from 'react-leaflet'
+import { Alert } from 'antd';
 import '../css/Map.css'
-import axios from 'axios'
+import http from '../common/http-common'
+
+const popupCamera = {
+  width: "200px",
+  height: "175px"
+};
 
 const Map = () => {
 
-    const [roadworks, setRoadworks] = useState([]);
+  const [roadworks, setRoadworks] = useState([]);
+  const [trafficNews, setTrafficNews] = useState([]);
+  const [loadingNews, setLoadingNews] = useState(true);
+  const [trafficCamera, setTrafficCamera] = useState([]);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const result = await axios.get(
-          'https://resource.data.one.gov.hk/td/roadworks-location/get_all_the_roadworks.json',
-        )
-        setRoadworks(result.data)
-      };
-      fetchData();
-    }, [setRoadworks]);
+  useEffect(
+    async () => {
+      try {
+      const res = await http.get('/roadworks')
+      setRoadworks(res.data)
+    } catch (err) {
+        console.log(err)
+    }
+  }, [])
   
+  useEffect(
+    async () => {
+      setLoadingNews(true)
+      try {
+      const res = await http.get('/trafficnews')
+      setTrafficNews(res.data.list.message)
+      } catch (err) {
+        console.log(err)
+      }
+      setLoadingNews(false)
+    }, [])
+  
+  useEffect(
+    async () => {
+      try {
+      const res = await http.get('/trafficcamera')
+      setTrafficCamera(res.data['image-list'].image)
+      } catch (err) {
+        console.log(err)
+      }
+    }, [])
+
+  console.log(loadingNews)
+
   return (
+    <>
+      {loadingNews == "true" ? (<></>):(<>
+      {trafficNews.INCIDENT_STATUS_EN == "CLOSED" ? ( <></> ) : ( <>
+        <Alert
+          message={<><b>{trafficNews.INCIDENT_HEADING_EN}</b>: {trafficNews.INCIDENT_DETAIL_EN} ({trafficNews.INCIDENT_STATUS_EN}: {trafficNews.ANNOUNCEMENT_DATE})</>}
+          description={trafficNews.CONTENT_EN}
+          banner
+          /></>)
+      } </> ) }
+    
     <MapContainer center={[22.2992961, 114.1894218]} zoom={14} scrollWheelZoom={true} minZoom={11}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-        <LayersControl position="topright">
-          <LayersControl.Overlay name="Roadworks">
-            <LayerGroup>
-              {roadworks && roadworks.map(rw =>(
-              <Marker 
-                key = {rw.roadworks_id}
+      <LayersControl position="topright">
+        <LayersControl.Overlay name="Roadworks">
+          <LayerGroup>
+            {roadworks && roadworks.map(rw => (
+              <Marker
+                key={rw.roadworks_id}
                 position={[rw.latitude, rw.longitude]}>
                 <Popup>
-                  {rw.workstype} ({rw.worksstatus})<br/>
-                  Start time: {rw.starttime}<br/>
+                  {rw.workstype} ({rw.worksstatus})<br />
+                  Start time: {rw.starttime}<br />
                   End time: {rw.endtime}
                 </Popup>
               </Marker>
-              ))}
-            </LayerGroup>
-          </LayersControl.Overlay>
-        </LayersControl>
+            ))}
+          </LayerGroup>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="Traffic Camera">
+          <LayerGroup>
+            {trafficCamera && trafficCamera.map(camera => (
+              <Marker
+                key={camera.key}
+                position={[camera.latitude, camera.longitude]}>
+                <Popup>
+                  <div style={popupCamera}>
+                  Key: {camera.key}<br />
+                  <img src={camera.url} width="205"/>
+                  </div>
+                </Popup>
+              </Marker>
+            ))}
+          </LayerGroup>
+        </LayersControl.Overlay>
+      </LayersControl>
     </MapContainer>
 
+</>
   )
 }
 
