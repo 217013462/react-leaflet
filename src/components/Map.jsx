3d0491c3d7d329bbac73e9674c8ee2a4b9aa3851
WY <101268150+217013462@users.noreply.github.com>
2022-05-19 06:57:32 +0000

    Added reset map view (home extend button)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import { MapContainer, TileLayer, useMap, LayersControl, LayerGroup } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, LayersControl, LayerGroup, ScaleControl } from 'react-leaflet'
+import L from 'leaflet'
+import ResetViewControl from '@20tab/react-leaflet-resetview'
 import '../css/Map.css'
 
 import Roadworks from './Roadworks'
 import TrafficCamera from './TrafficCamera'
 import TrafficNews from './TrafficNews'
 
-const Map = () => {
+const center = [22.2992961, 114.1894218]
+const zoom = 14
 
+const Map = () => {
+  
   return (
     <>
       <TrafficNews />
-      <MapContainer center={[22.2992961, 114.1894218]} zoom={14} scrollWheelZoom={true} minZoom={11}>
+      <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} minZoom={11}>
+        <ResetViewControl
+          title="Home Extend"
+          icon={"🌎"}
+          />
+        <ScaleControl imperial={false} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
